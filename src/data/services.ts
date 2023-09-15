cb1f4f0774459fb2c4480e0a2634bf2e4390b57b
sabahrahal <sabahrahal1998@gmail.com>
2023-09-16 00:41:40 +0200

    fix routes of static files

--- src/data/services.ts
+++ src/data/services.ts
@@ -2,61 +2,61 @@ export const services = [
     {
         title: 'Baños',
         description: 'Renovación de baños',
-        icon: '../files/icons/bano.webp'
+        icon: '/icons/bano.webp'
     },
     {
         title: 'Cocinas',
         description: 'Diseño de cocinas',
-        icon: '../files/icons/cocina.webp'
+        icon: '/icons/cocina.webp'
     },
     {
         title: 'Puertas',
         description: 'Puertas de calidad',
-        icon: '../files/icons/puerta.webp'
+        icon: '/icons/puerta.webp'
     },
     {
         title: 'Pintura',
         description: 'Expertos en pintura',
-        icon: '../files/icons/pintura.webp'
+        icon: '/icons/pintura.webp'
     },
     {
         title: 'Climatización',
         description: 'Aire acondicionado',
-        icon: '../files/icons/climatizacion.webp'
+        icon: '/icons/climatizacion.webp'
     },
     {
         title: 'Parquet',
         description: 'Suelos de parquet',
-        icon: '../files/icons/parquet.webp'
+        icon: '/icons/parquet.webp'
     },
     {
         title: 'Aluminio',
         description: 'Carpintería de aluminio',
-        icon: '../files/icons/aluminio.webp'
+        icon: '/icons/aluminio.webp'
     },
     {
         title: 'Proyectos',
         description: 'Planificación profesional',
-        icon: '../files/icons/proyecto.webp'
+        icon: '/icons/proyecto.webp'
     },
     {
         title: 'Pladur',
         description: 'Tabiquería en pladur',
-        icon: '../files/icons/pladur.webp'
+        icon: '/icons/pladur.webp'
     },
     {
         title: 'Renders 3D',
         description: 'Visualización 3D',
-        icon: '../files/icons/3d.webp'
+        icon: '/icons/3d.webp'
     },
     {
         title: 'Diseño Personalizado',
         description: 'Diseño a medida',
-        icon: '../files/icons/diseno.webp'
+        icon: '/icons/diseno.webp'
     },
     {
         title: 'Instlación Electrica',
         description: 'Electricidad segura',
-        icon: '../files/icons/instalacion-electrica.webp'
+        icon: '/icons/instalacion-electrica.webp'
     },
 ]
