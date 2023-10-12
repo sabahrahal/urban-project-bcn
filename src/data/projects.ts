6f8b3e91531b9f789704f787dd607155b3c3f59f
sabahrahal <sabahrahal1998@gmail.com>
2023-10-12 19:20:17 +0200

    fix: :bug: fix deply images src

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ export const projectsData = [
         title: 'Baños',
         category: 'banos',
         categoryId: 'GqwIxfr',
-        icon: '/icons/bano.webp',
+        icon: '/bano.webp',
         description: 'Descubre nuestra experiencia en la creación de baños funcionales y elegantes. Transformamos espacios de aseo en oasis de relajación y comodidad, cuidando cada detalle.',
     },
 
@@ -11,7 +11,7 @@ export const projectsData = [
         title: 'Interiores',
         category: 'interiores',
         categoryId: 'UzVvKsd',
-        icon: '/icons/parquet.webp',
+        icon: '/parquet.webp',
         description: 'Explora nuestros proyectos de diseño de interiores que fusionan estilo y funcionalidad. Creamos ambientes acogedores y estéticamente atractivos.',
     },
 
@@ -19,7 +19,7 @@ export const projectsData = [
         title: 'Cocinas',
         category: 'cocinas',
         categoryId: 'x6oiPPc',
-        icon: '/icons/cocina.webp',
+        icon: '/cocina.webp',
         description: 'Sumérgete en nuestras innovadoras cocinas. Diseñamos espacios culinarios que hacen que cocinar sea un placer y que se adaptan a tu estilo de vida.',
     },
 
@@ -27,7 +27,7 @@ export const projectsData = [
         title: 'Iluminación Led',
         category: 'iluminacion',
         categoryId: 'ckakE6K',
-        icon:'/icons/instalacion-electrica.webp',
+        icon:'/instalacion-electrica.webp',
         description: 'Experimenta la magia de la iluminación LED en nuestros proyectos. Transformamos espacios con tecnología eficiente que resalta la belleza de tu entorno y ahorra energía.',
     }
 ]
--- src/data/services.ts
+++ src/data/services.ts
@@ -2,61 +2,61 @@ export const services = [
     {
         title: 'Baños',
         description: 'Renovación de baños',
-        icon: '/icons/bano.webp'
+        icon: '/bano.webp'
     },
     {
         title: 'Cocinas',
         description: 'Diseño de cocinas',
-        icon: '/icons/cocina.webp'
+        icon: '/cocina.webp'
     },
     {
         title: 'Puertas',
         description: 'Puertas de calidad',
-        icon: '/icons/puerta.webp'
+        icon: '/puerta.webp'
     },
     {
         title: 'Pintura',
         description: 'Expertos en pintura',
-        icon: '/icons/pintura.webp'
+        icon: '/pintura.webp'
     },
     {
         title: 'Climatización',
         description: 'Aire acondicionado',
-        icon: '/icons/climatizacion.webp'
+        icon: '/climatizacion.webp'
     },
     {
         title: 'Parquet',
         description: 'Suelos de parquet',
-        icon: '/icons/parquet.webp'
+        icon: '/parquet.webp'
     },
     {
         title: 'Aluminio',
         description: 'Carpintería de aluminio',
-        icon: '/icons/aluminio.webp'
+        icon: '/aluminio.webp'
     },
     {
         title: 'Proyectos',
         description: 'Planificación profesional',
-        icon: '/icons/proyecto.webp'
+        icon: '/proyecto.webp'
     },
     {
         title: 'Pladur',
         description: 'Tabiquería en pladur',
-        icon: '/icons/pladur.webp'
+        icon: '/pladur.webp'
     },
     {
         title: 'Renders 3D',
         description: 'Visualización 3D',
-        icon: '/icons/3d.webp'
+        icon: '/3d.webp'
     },
     {
         title: 'Diseño Personalizado',
         description: 'Diseño a medida',
-        icon: '/icons/diseno.webp'
+        icon: '/diseno.webp'
     },
     {
         title: 'Instalaciones',
         description: 'Agua, gas y electricidad',
-        icon: '/icons/instalaciones.webp'
+        icon: '/instalaciones.webp'
     },
 ]
