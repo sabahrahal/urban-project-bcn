f89c80a7fe9c426a52a4f224f3ec955d84f3ed8b
sabahrahal <sabahrahal1998@gmail.com>
2023-10-12 18:28:41 +0200

    feat: :sparkles:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+  site: 'https://urbanprojectbcn.com',
+  integrations: [tailwind(), robotsTxt()]
 });
